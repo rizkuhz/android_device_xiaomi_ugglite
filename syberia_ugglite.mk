a2647205dd3564ed5a0bb63334614ec9f8a1d01c
adislice <romadhonadipermana279f@gmail.com>
2020-05-13 08:00:50 +0000

    ugglite: Add prebuilt APN list and update tree
    Signed-off-by: adislice <romadhonadipermana279f@gmail.com>

--- syberia_ugglite.mk
+++ syberia_ugglite.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Syberia stuff
-$(call inherit-product, vendor/syberia/config/common.mk)
+$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
 
 # Inherit from ugglite device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
